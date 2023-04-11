6966d75bd2c566836e7b37956c988dfae58927a1
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-04-11 22:57:27 +0100

    greetings

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,11 +1,29 @@
 import { Avatar, Card, Typography, Box } from "@mui/material";
+import { useState, useEffect } from "react";
 import styles from '@/styles/Home.module.css';
 
 const UserCard = () => {
+    const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const hour = time.getHours();
+
+  let greeting;
+  if (hour >= 5 && hour < 12) {
+    greeting = "Good morning";
+  } else if (hour >= 12 && hour < 18) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
     return (
         <Card className={styles.userCard} elevation={5}>
             <Box className={styles.cardDetail}>
-            <Typography variant="h6" className={styles.greetText}>Good Morning John Doe</Typography>
+            <Typography variant="h6" className={styles.greetText}>{greeting} John Doe</Typography>
             <Avatar sx={{
                 backgroundColor:'#b222ea'
             }}>J</Avatar>
